93365af0c9578580efe8a904c9d20bdd4190b90e
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2023-12-26 10:27:03 -0500

    making some changes, got error at 3001 port

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // Require directories/libraries
 const express = require('express');
 const path = require('path');
-// uuid function for creating new id # for each note
+// fs for writing new files/items
+const fs = require('fs');
+// UUID see README for resource
 const { v4: uuidv4 } = require('uuid');
 const app = express();
 
 // Start program on PORT location
-// allow choice if 3001 taken
 const PORT = process.env.PORT || 3001;
 
 // GET request for HTTP
@@ -22,14 +23,12 @@ app.get('*', (req,res) => {
 // POST req,res
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
-    newNote.id = uuid4();
+    newNote.id = uuidv4();
     // Get existing notes from the db.json file
     const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db', 'db.json'), 'utf8'));
-    
-    // Will need to add new notes to existing location as PUSH
     // Add the new note to the array
     notes.push(newNote);
-    // Write the updated info back to the db.json file
+    // Write the updated array back to the db.json file
     fs.writeFileSync(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes));
     res.json(newNote); // Return the new note to the client
 });
