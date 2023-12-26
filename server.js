1c09e7778b8a67a74e9502c49ee3cdbf22d766b0
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2023-12-26 10:07:22 -0500

    expanded prior code for new note items

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 
 // Start program on PORT location
-const PORT = 3001;
+// allow choice if 3001 taken
+const PORT = process.env.PORT || 3001;
 
 // GET request for HTTP
 app.get('./notes', (req, res) => {
@@ -22,11 +23,23 @@ app.get('*', (req,res) => {
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
     newNote.id = uuid4();
-})
+    // Get existing notes from the db.json file
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db', 'db.json'), 'utf8'));
+    
+    // Will need to add new notes to existing location as PUSH
+    // Add the new note to the array
+    notes.push(newNote);
+    // Write the updated info back to the db.json file
+    fs.writeFileSync(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes));
+    res.json(newNote); // Return the new note to the client
+});
 
-
-// Will need to add new notes to existing location as PUSH
-
-
+// Redirect all other routes to the homepage
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Start program
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
